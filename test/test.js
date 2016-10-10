84d390ca3c98e18f67ec17a0db0f3d9a978313e8
Stephen Cresswell <stephen.cresswell@guidesmiths.com>
2016-10-10 13:13:43 +0100

    bye bye ES6

--- test/test.js
+++ test/test.js
@@ -3,16 +3,16 @@ var prerelease = require('..')
 
 describe('Prerelease', function() {
 
-    it('should return the original version with prerelease', () => {
+    it('should return the original version with prerelease', function() {
         assert.equal(prerelease('1.2.3', 'alpha', 1), '1.2.3-alpha.1')
     })
 
-    it('should strip previous prerelease', () => {
+    it('should strip previous prerelease', function() {
         assert.equal(prerelease('1.2.3-1', 'beta', 2), '1.2.3-beta.2')
         assert.equal(prerelease('1.2.3-alpha.1', 'beta', 2), '1.2.3-beta.2')
     })
 
-    it('should strip previous build', () => {
+    it('should strip previous build', function() {
         assert.equal(prerelease('1.2.3+1', 'beta', 2), '1.2.3-beta.2')
         assert.equal(prerelease('1.2.3+alpha.1', 'beta', 2), '1.2.3-beta.2')
     })
